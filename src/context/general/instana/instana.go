8bb7e18b1abd28f7373cf9430f36a2e8a2d41ded
Luis Felipe <godoiluisfelipe@gmail.com>
2021-04-01 19:42:28 -0300

    feat: added instana context

---
+++ src/context/general/instana/instana.go
@@ -0,0 +1,21 @@
+package instana
+
+import (
+	instana "github.com/instana/go-sensor"
+	"github.com/sirupsen/logrus"
+)
+
+type Instana struct {
+	Logger *logrus.Logger
+}
+
+func NewInstana(logger *logrus.Logger) Instana {
+	return Instana{
+		Logger: logger,
+	}
+}
+
+func (it *Instana) InitMetrics() {
+	instana.InitSensor(&instana.Options{})
+	instana.SetLogger(it.Logger)
+}
